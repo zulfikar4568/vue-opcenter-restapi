8ce2471e70d2cc607d66e6bce268563e0faf7923
zulfikar4568 <isnaen70@gmail.com>
2024-02-25 00:57:08 +0700

    feat: form factory

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,55 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
+import Form from './components/FormFactory.vue';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons-vue';
 
 interface DataItem {
-  InstanceID: string;
   Name: string;
   Description: string;
 }
 const data: DataItem[] = [
   {
-    InstanceID: '1',
     Name: 'Factory A',
     Description: 'This is a factory',
   },
   {
-    InstanceID: '2',
     Name: 'Factory B',
     Description: 'This is a factory',
   },
 ];
 
+const isCreate = ref<boolean>(false);
 const open = ref<boolean>(false);
-const showDrawer = () => {
+const showDrawer = (create: boolean) => {
   open.value = true;
+  isCreate.value = create;
 };
+
+const detailData = reactive<DataItem>({
+  Name: '',
+  Description: '',
+})
 </script>
 
 <template>
   <div class="container">
     <a-divider>Factory</a-divider>
-    <a-button @click="showDrawer" type="primary" class="pull-right">Create</a-button>
+    <a-button @click="showDrawer(true)" type="primary" class="pull-right">Create</a-button>
     <a-list item-layout="horizontal" :data-source="data">
       <template #renderItem="{ item }">
         <a-list-item>
+          <template #actions>
+            <a-button @click="showDrawer(false)" key="list-loadmore-edit" type="primary">
+              <template #icon>
+                <EditOutlined />
+              </template>
+            </a-button>
+            <a-button key="list-loadmore-delete" type="primary" danger>
+              <template #icon>
+                <DeleteOutlined />
+              </template>
+            </a-button>
+          </template>
           <a-list-item-meta :description="item.Description">
             <template #title>
               <b>{{ item.Name }}</b>
@@ -43,11 +61,9 @@ const showDrawer = () => {
   </div>
   <a-drawer
     v-model:open="open"
-    title="Create Factory"
-    placement="bottom"
+    :title="isCreate ? 'Create Factory' : 'Update Factory'"
+    placement="left"
   >
-    <p>Some contents...</p>
-    <p>Some contents...</p>
-    <p>Some contents...</p>
+    <Form :name="detailData.Name" :title="isCreate ? 'Create Factory' : 'Update Factory'" :description="detailData.Description" :is-create="isCreate" />
   </a-drawer>
 </template>
---
+++ src/components/FormFactory.vue
@@ -0,0 +1,60 @@
+<template>
+  <a-form
+    :model="data"
+    name="basic"
+    autocomplete="off"
+    @finish="onFinish"
+    :layout="'vertical'"
+  >
+    <a-form-item
+      label="Factory Name"
+      name="name"
+      :rules="[{ required: true, message: 'Please input factory name!' }]"
+    >
+      <a-input v-model:value="data.name" />
+    </a-form-item>
+
+    <a-form-item
+      label="Description"
+      name="description"
+      :rules="[{ required: false, message: 'Please input factory description!' }]"
+    >
+      <a-input v-model:value="data.description" />
+    </a-form-item>
+
+    <a-form-item>
+      <a-button type="primary" html-type="submit">Submit</a-button>
+    </a-form-item>
+  </a-form>
+</template>
+<script lang="ts" setup>
+import { reactive } from 'vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  isCreate: {
+    type: Boolean,
+    required: true,
+  }
+})
+
+const data = reactive({
+  name: props.name,
+  description: props.description
+})
+
+const onFinish = (values: any) => {
+  console.log('Success:', values);
+};
+</script>
